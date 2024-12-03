e5f6b86ed78fbd9745839e48a82fd680ec7f68b2
GuusDb <79090456+GuusDb@users.noreply.github.com>
2024-12-03 14:56:18 +0100

    advent of code part 2

--- AocDay3/Multiplier.cs
+++ AocDay3/Multiplier.cs
@@ -4,16 +4,38 @@ namespace AocDay3;
 public static class Multiplier
 {
     private static int Mul(int x, int y) => x * y;
+
     public static int AdderMultiplierForLine(string line)
     {
         var lineTotal = 0;
+        var isEnabled = true;
 
-        string pattern = @"mul\((\d+),(\d+)\)";
+        string mulPattern = @"mul\((\d+),(\d+)\)";
+        string doPattern = @"\bdo\(\)";
+        string dontPattern = @"\bdon't\(\)";
 
-        var matches = Regex.Matches(line, pattern);
+        string combinedPattern = $@"{mulPattern}|{doPattern}|{dontPattern}";
+        var matches = Regex.Matches(line, combinedPattern);
+
         foreach (Match match in matches)
         {
-            lineTotal += Mul(int.Parse(match.Groups[1].Value!), int.Parse(match.Groups[2].Value!));
+            if (match.Groups[1].Success && match.Groups[2].Success)
+            {
+                if (isEnabled)
+                {
+                    int x = int.Parse(match.Groups[1].Value);
+                    int y = int.Parse(match.Groups[2].Value);
+                    lineTotal += Mul(x, y);
+                }
+            }
+            else if (Regex.IsMatch(match.Value, doPattern))
+            {
+                isEnabled = true;
+            }
+            else if (Regex.IsMatch(match.Value, dontPattern))
+            {
+                isEnabled = false;
+            }
         }
 
         return lineTotal;
