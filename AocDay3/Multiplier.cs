0236f2bba7debced02dd68c12a3273273cac4b1f
GuusDb <79090456+GuusDb@users.noreply.github.com>
2024-12-03 13:12:37 +0100

    advent of code day 3

---
+++ AocDay3/Multiplier.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AocDay3;
+
+public static class Multiplier
+{
+    private static int Mul(int x, int y) => x * y;
+
+    public static int AdderMultiplierForLine(string line)
+    {
+        var lineTotal = 0;
+
+        var mulPattern = @"mul\((\d+),(\d+)\)";
+        var controlPattern = @"do\(\)|don't\(\)";
+
+        var IsMulEnabled = true;
+
+        var combinedPattern = $@"{controlPattern}|{mulPattern}";
+
+        var matches = Regex.Matches(line, combinedPattern);
+
+        foreach (Match match in matches)
+        {
+            if (match.Value.StartsWith("do()"))
+            {
+                IsMulEnabled = true;
+            }
+            else if (match.Value.StartsWith("don't()"))
+            {
+                IsMulEnabled = false;
+            }
+            else if (IsMulEnabled && match.Value.StartsWith("mul("))
+            {
+                int x = int.Parse(match.Groups[1].Value!);
+                int y = int.Parse(match.Groups[2].Value!);
+                lineTotal += Mul(x, y);
+            }
+        }
+
+        return lineTotal;
+    }
+}
---
+++ AocDay3/Program.cs
@@ -0,0 +1,31 @@
+﻿using AocDay3;
+
+var total = 0;
+
+try
+{
+    string filePath = Path.Combine("..\\..\\..\\text.txt");
+    if (!File.Exists(filePath))
+    {
+        Console.WriteLine($"File not found: {filePath}");
+        return;
+    }
+
+    using StreamReader sr = new StreamReader(filePath);
+    string? line;
+
+    while ((line = sr.ReadLine()) != null)
+    {
+        total += Multiplier.AdderMultiplierForLine(line);
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Exception: {e.Message}");
+}
+finally
+{
+    Console.WriteLine("Finished processing file.");
+}
+
+Console.WriteLine($"Total of the muiltiplication: {total}");
